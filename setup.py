8c4cc6a7f9013bd8dadd9d4e14b727ddd7f48df1
JT Raber <JMANN240raber@gmail.com>
2022-10-27 17:04:41 -0400

    added a setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+# This is a file that should be run every time there is a change in the database schemas or when one wants to clear their copy of the database
+
+import sqlite3
+
+with sqlite3.connect("databse.db") as connection: # Get a connection to the database file
+	cursor = connection.cursor() # Get a cursor so we can modify the database
+
+	# For every table we need to drop it if it exists and then create it.
+
+	cursor.execute('DROP TABLE IF EXISTS blah')
+	cursor.execute('''
+		CREATE TABLE blah (
+			col1 INTEGER PRIMARY KEY,
+			col2 TEXT NOT NULL
+			col3 TEXT NOT NULL
+		)
+	''')
+
+	# Then fill in our entries
+
+	cursor.execute('INSERT INTO blah (col1, col2, col3) VALUES (1, "foo", "bar")')
