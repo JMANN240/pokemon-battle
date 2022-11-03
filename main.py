5db83ebe58a08cf6d09f849f47ff4df342090e43
JT Raber <JMANN240raber@gmail.com>
2022-11-03 12:50:26 -0400

    fixed some file structure

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import sqlite3
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+api = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount('/api', api, name='api')
 
 templates = Jinja2Templates(directory='templates')
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/items', response_class=HTMLResponse)
 async def index(request: Request):
-	return templates.TemplateResponse('index.html', {'request': request})
+	return templates.TemplateResponse('items.html', {'request': request})
