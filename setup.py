88eec090eba2ed714273af4962db99cb1b6e3dba
Yaseen <yshaikh@kent.edu>
2022-11-12 12:03:52 -0500

    added to setup.py, there's a syntax error i haven't figured out though

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ with sqlite3.connect("database.db") as connection: # Get a connection to the dat
 	# For every table we need to drop it if it exists and then create it.
 
 	cursor.execute('DROP TABLE IF EXISTS item_types')
+	cursor.execute('DROP TABLE IF EXISTS item')
+	cursor.execute('DROP TABLE IF EXISTS player')
+	cursor.execute('DROP TABLE IF EXISTS pokemon_types')
+
 	cursor.execute('''
+		
 		CREATE TABLE item_types
 		(
 			type_id INT NOT NULL,
@@ -24,6 +29,18 @@ with sqlite3.connect("database.db") as connection: # Get a connection to the dat
   			PRIMARY KEY (item_id),
   			FOREIGN KEY (type_id) REFERENCES item_types(type_ID)
 		)
+		CREATE TABLE player
+		(
+  			player_id INT NOT NULL,
+  			player_name TEXT NOT NULL,
+  			PRIMARY KEY (player_id)
+		)
+		CREATE TABLE pokemon_types
+		(
+  			p_type_id INT NOT NULL,
+  			p_type_name TEXT NOT NULL,
+  			PRIMARY KEY (p_type_id)
+		)
 
 	''')
 
@@ -32,5 +49,24 @@ with sqlite3.connect("database.db") as connection: # Get a connection to the dat
 	cursor.execute('INSERT INTO item_types (type_id, type_name) VALUES (0, "potions")')
 	cursor.execute('INSERT INTO item_types (type_id, type_name) VALUES (1, "balls")')
 	cursor.execute('INSERT INTO item_types (type_id, type_name) VALUES (2, "special")')
-	cursor.execute('INSERT INTO item (item_id, item_name, color, type_id) VALUES (0, "regular_ball", "red", 1)
+	cursor.execute('INSERT INTO item (item_id, item_name, color, type_id) VALUES (0, "regular_ball", "red", 1)')
+	cursor.execute('INSERT INTO player (player_id, player_name) VALUES (0, "bob")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (0, "bug")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (1, "dark")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (2, "dragon")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "electric")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "fairy")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "fighting")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "fire")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "flying")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "ghost")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "grass")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "ground")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "ice")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "normal")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "poison")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "psychic")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "rock")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "steel")')
+	cursor.execute('INSERT INTO pokemon_types (p_type_id, p_type_name) VALUES (3, "water")')
 	connection.commit()
